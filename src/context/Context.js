bc3a542ca99c65008f72caa8acd1b20ae8d4128c
26haroon26 <26haroon26@gmail.com>
2023-01-20 20:48:01 +0500

    railway

--- src/context/Context.js
+++ src/context/Context.js
@@ -10,7 +10,7 @@ let data = {
   baseUrl:
     window.location.href.split(":")[0] === "http"
       ? "http://localhost:4000/api/v1"
-      : "https://sore-blue-goose.cyclic.app/api/v1",
+      : "twiteerserver-production.up.railway.app/api/v1",
 };
 
 export default function ContextProvider({ children }) {
